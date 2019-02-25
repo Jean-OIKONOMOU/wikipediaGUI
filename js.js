62d23b425d1df21a9163a5070f695a50184ac46d
Jean-Adrian OIKONOMOU <Jean.oikonomou@gmail.com>
2019-02-25 15:47:31 +0100

    tableau prêt

--- js.js
+++ js.js
@@ -6,73 +6,95 @@ request.send();
 request.onload = function() {
   if (request.readyState === 4 && request.status === 200) {
     console.log("Success"); // JSON DATA HAS BEEN RECEIVED SUCCESSFULLY AND IS READY FOR WORK
-    var x = request.responseText; // EXTRACT THE RAX TEXT FROM THE JSON
-    document.getElementById("id01").innerHTML = x; // DISPLAY RAW TEXT IN HTML
+  //  var x = request.responseText; // EXTRACT THE RAX TEXT FROM THE JSON
+  //  document.getElementById("id01").innerHTML = x; // DISPLAY RAW TEXT IN HTML
 
-/* ARRAY ELEMENTS :
-history TH
-image TH
-profile TD
-firstname TD (concatenate with lastname)
-lastname TD
-wiki
-*/
+    /* ARRAY ELEMENTS :
+    history TH
+    image TH
+    profile TD
+    firstname TD (concatenate with lastname)
+    lastname TD
+    wiki
+    */
 
-  // EXTRACTING THE DATA ELEMENT BY ELEMENT
-jayson = JSON.parse(request.response);
-console.log(jayson[0].history);
-console.log(jayson.length);
-console.log(jayson[0].image);
-console.log(jayson[0].wiki);
-console.log(jayson[0].profile);
-console.log(jayson[0].profile.firstname);
-console.log(jayson[0].profile.lastname);
+    // EXTRACTING THE DATA ELEMENT BY ELEMENT
+    jayson = JSON.parse(request.response);
+    console.log(jayson[0].history);
+    console.log(jayson.length);
+    console.log(jayson[0].image);
+    console.log(jayson[0].wiki);
+    console.log(jayson[0].profile);
+    console.log(jayson[0].profile.firstname);
+    console.log(jayson[0].profile.lastname);
 
- for (var i = 0; i < jayson.length; i++) {
-   console.log(jayson[i].wiki);
- }
-
-var table;
+  /*  var table;
     // CREATING TABLE
-  for (i = 0; i < jayson.length; i++) {
-  table="<table>";
-  table+="<tr><td>Name</td><td>History</td></tr>";
-  table+="<tr><td>"+jayson[i].profile.firstname+ " " + jayson[i].profile.lastname+"</td><td>"+jayson[i].history+"</td></tr>";
-  table+="</table>";
-  document.getElementById("id01").innerHTML = table;
-}
-// creates a <table> element and a <tbody> element
-var tbl = document.createElement("table");
-var tblBody = document.createElement("tbody");
+    for (i = 0; i < jayson.length; i++) {
+      table = "<table>";
+      table += "<tr><td>Name</td><td>History</td></tr>";
+      table += "<tr><td>" + jayson[i].profile.firstname + " " + jayson[i].profile.lastname + "</td><td>" + jayson[i].history + "</td></tr>";
+      table += "</table>";
+      document.getElementById("id01").innerHTML = table;
+    }*/
 
-// creating all cells
-for (var i = 0; i < jayson.length; i++) {
-  // creates a table row
-  var row = document.createElement("tr");
 
-  for (var j = 0; j < 3; j++) {
-    // Create a <td> element and a text node, make the text
-    // node the contents of the <td>, and put the <td> at
-    // the end of the table row
-    var cell = document.createElement("td");
+    // creates a <table> element and a <tbody> element
+    var tbl = document.createElement("table");
+    tbl.style.margin = "auto 0 auto 250px";
+    var tblBody = document.createElement("tbody");
 
-    var cellText = document.createTextNode(jayson[i].profile.firstname+ " " + jayson[i].profile.lastname);
-    cellText.id = i;
-    cellText.href = jayson[i].wiki;
-    cellText.title = jayson[i].wiki;
-    cell.appendChild(cellText);
-    row.appendChild(cell);
-    cell.style.border = "2px dashed blue";
-  }
+    // creating all cells
+    for (i = 0; i < jayson.length; i++) {
 
-  // add the row to the end of the table body
-  tblBody.appendChild(row);
-}
+      // creates a table row
+      var row = document.createElement("tr");
 
-// put the <tbody> in the <table>
-tbl.appendChild(tblBody);
-// appends <table> into <body>
-document.getElementById("id01").appendChild(tbl);
+      for (var j = 0; j < 1; j++) {
+        // Create a <td> element and a text node, make the text
+        // node the contents of the <td>, and put the <td> at
+        // the end of the table row
+        var cell1 = document.createElement("td");
+        var a = document.createElement("a");
+        a.setAttribute("href", jayson[i].wiki);
+        a.setAttribute("target", "_blank");
+        a.innerHTML = jayson[i].profile.firstname + " " + jayson[i].profile.lastname;
+        cell1.appendChild(a);
+        row.appendChild(cell1);
+        cell1.style.border = "2px dashed red";
+        cell1.style.padding = '15px';
+      }
+
+      for (j = 0; j < 1; j++) {
+        var cell2 = document.createElement("td");
+        var history = document.createTextNode(jayson[i].history);
+        cell2.appendChild(history);
+        row.appendChild(cell2);
+        cell2.style.border = "2px dashed blue";
+        cell2.style.width = '200px';
+        cell2.style.padding = '15px';
+        cell2.style.textAlign = "center";
+      }
+
+      for (j = 0; j < 1; j++) {
+        var cell3 = document.createElement("td");
+        var img = document.createElement("img");
+        img.setAttribute("src", jayson[i].image);
+        img.setAttribute("width", "50%");
+        img.setAttribute("height", "40%");
+        cell3.appendChild(img);
+        row.appendChild(cell3);
+      //  cell3.style.border = "2px dashed green";
+      }
+
+      // add the row to the end of the table body
+      tblBody.appendChild(row);
+    }
+
+    // put the <tbody> in the <table>
+    tbl.appendChild(tblBody);
+    // appends <table> into <body>
+    document.getElementById("id01").appendChild(tbl);
   }
 };
 
